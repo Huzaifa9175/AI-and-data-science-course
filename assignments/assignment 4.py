bae66d1e7d20dfdda4630ce71eadabfc30b15023
huzzi3330-cell <huzzi3330@gmail.com>
2026-08-18 02:32:33 +0500

    Add weather app

--- assignments/assignment 4.py
+++ assignments/assignment 4.py
@@ -1,29 +1,24 @@
 Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
 >>> 
-================== RESTART: C:/Users/USAMA/Desktop/practice.py =================
-=================
-1.Add Student
-2.Display Student
-3.Search Student
-4.Update Student
-5.Delete Student
-6.Sort Student
-7.Exit
-=================
-Enter your choice :1
-Enetr student name :kayum
-Enetr student roll no. :em23
-Enetr student course :SMIT
-Enetr student marks :100
-========================
-Record added succefully 
-=================
-1.Add Student
-2.Display Student
-3.Search Student
-4.Update Student
-5.Delete Student
-6.Sort Student
-7.Exit
-=================
+= RESTART: C:/Users/USAMA/AppData/Local/Programs/Python/Python313/assignment 4.py
+
+===== STUDENT RECORDS MANAGEMENT SYSTEM =====
+1. Add Student
+2. Display Students
+3. Search Student
+4. Update Record
+5. Delete Record
+6. Sort Records by Marks
+7. Exit
+Enter your choice: 
+= RESTART: C:/Users/USAMA/AppData/Local/Programs/Python/Python313/assignment 4.py
+
+===== STUDENT RECORDS MANAGEMENT SYSTEM =====
+1. Add Student
+2. Display Students
+3. Search Student
+4. Update Record
+5. Delete Record
+6. Sort Records by Marks
+7. Exit
